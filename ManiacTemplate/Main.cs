1dcf1549135fbc737c936a91771254bd1e2a7b1b
BoliBerrys <kiper30@gmail.com>
2017-06-15 01:29:51 -0300

    AutoSpell Leveler
    Added AutoSpell Lelever

--- ManiacTemplate/Main.cs
+++ ManiacTemplate/Main.cs
@@ -1,6 +1,7 @@
 ﻿using HesaEngine.SDK;
 using HesaEngine.SDK.Enums;
 using ManiacTemplate.Modes;
+using static HesaEngine.SDK.ObjectManager;
 using static ManiacTemplate.SpellManager;
 using static ManiacTemplate.MenuManager;
 using static ManiacTemplate.DrawingManager;
@@ -10,7 +11,7 @@ namespace ManiacTemplate
     public class Main : IScript
     {
         //Don't forget to change the champion first!
-        private const Champion Champion = HesaEngine.SDK.Enums.Champion.Ashe;
+        private const Champion Champion = HesaEngine.SDK.Enums.Champion.Fiora;
 
         public string Name => "Maniac" + Champion;
 
@@ -29,24 +30,21 @@ namespace ManiacTemplate
 
         private void Game_OnGameLoaded()
         {
-            if (ObjectManager.Me.Hero != Champion)
+            if (Me.Hero != Champion)
                 return;
-
             LoadMenu();
-
             LoadSpells();
-
             HesaEngine.SDK.AntiGapcloser.OnEnemyGapcloser += Modes.AntiGapcloser.DoAntigapclose;
-
             LoadDrawings();
-
             Game.OnTick += Game_OnTick;
-            Chat.Print(Name + " Loaded Successfully");
+            Chat.Print(Name + " Loaded Successfullyxxasd");
         }
+
+        private static int _currentLevel = 1;
 
         private static void Game_OnTick()
         {
-            var mana = ObjectManager.Me.ManaPercent;
+            var mana = Me.ManaPercent;
 
             if (KillstealMenu.GetCheckbox("enable"))
                 Killsteal.DoKs();
@@ -65,7 +63,11 @@ namespace ManiacTemplate
 
             if (Orb.ActiveMode == Orbwalker.OrbwalkingMode.Flee && mana >= FleeMenu.GetSlider("mana"))
                 Flee.DoFlee();
-            
+
+            //This is a basic fix, due to OnLevelUp being disabled
+            if (_currentLevel == Player.Level) return;
+            Leveler();
+            _currentLevel = Player.Level;
         }
     }
 }
--- ManiacTemplate/SpellManager.cs
+++ ManiacTemplate/SpellManager.cs
@@ -1,5 +1,10 @@
-﻿using HesaEngine.SDK;
+﻿using System;
+using System.Data.Common;
+using System.Media;
+using HesaEngine.SDK;
 using HesaEngine.SDK.Enums;
+using HesaEngine.SDK.GameObjects;
+using static ManiacTemplate.MenuManager;
 
 namespace ManiacTemplate
 {
@@ -19,9 +24,7 @@ namespace ManiacTemplate
             E.SetCharged(spellName: "", buffName: "", minRange: 100, maxRange: 1000, deltaT: 14.2f);
             R.SetTargetted(delay: 1, speed: 2000);
 
-
-
-
+            
             //Do the same with QPred, EPred, RPred, always depending on what they are going to collide.
             //Examples: 
             //Jhin's W. Collides with Heroes, and YasuoWall. But not with Minions.
@@ -40,6 +43,109 @@ namespace ManiacTemplate
                     CollisionableObjects.YasuoWall
                 }
             };
+            //Disabled since 7.10
+            //AIHeroClient.OnLevelUp += AIHeroClient_OnLevelUp;
+        }
+
+        private static void AIHeroClient_OnLevelUp(AIHeroClient sender, int level)
+        {
+            if (!MiscMenu.GetCheckbox("level") || !sender.IsMe) return;
+            var delay = MiscMenu.GetSlider("levelDelay");
+
+            Core.DelayAction(AutoLeveler, delay);
+        }
+
+        public static void Leveler()
+        {
+            if (!MiscMenu.GetCheckbox("level")) return;
+            var delay = MiscMenu.GetSlider("levelDelay");
+
+            Core.DelayAction(AutoLeveler, delay);
+        }
+
+        private static void AutoLeveler()
+        {
+            if (R.CanLevel())
+                ObjectManager.Player.Spellbook.LevelUpSpell(SpellSlot.R);
+
+            var first = GetSlot(MiscMenu.GetCombobox("levelFirst"));
+            var second = GetSlot(MiscMenu.GetCombobox("levelSecond"));
+            var third = GetSlot(MiscMenu.GetCombobox("levelThird"));
+
+            if(ObjectManager.Player.Level == 4 && ObjectManager.Player.Spellbook.GetSpell(third).Slot.CanLevel() && ObjectManager.Player.Spellbook.GetSpell(third).Level <= 0)
+                ObjectManager.Player.Spellbook.LevelUpSpell(third);
+
+            if (ObjectManager.Player.Spellbook.GetSpell(first).Slot.CanLevel())
+                ObjectManager.Player.Spellbook.LevelUpSpell(first);
+                
+            if (ObjectManager.Player.Spellbook.GetSpell(second).Slot.CanLevel())
+                ObjectManager.Player.Spellbook.LevelUpSpell(second);
+
+            if (ObjectManager.Player.Spellbook.GetSpell(third).Slot.CanLevel())
+                ObjectManager.Player.Spellbook.LevelUpSpell(third);
+
+        }
+
+        private static SpellSlot GetSlot(this int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    return SpellSlot.Q;
+                case 1:
+                    return SpellSlot.W;
+                case 2:
+                    return SpellSlot.E;
+            }
+            Chat.Print("Failed to get SpellSlot");
+            return SpellSlot.Unknown;
+        }
+
+        public static double GetSpellDamage(this Obj_AI_Base target, SpellSlot slot)
+        {
+            var slotLevel = ObjectManager.Player.GetSpell(slot).Level - 1;
+            var ap = ObjectManager.Player.FlatMagicDamageMod;
+            var ad = ObjectManager.Player.FlatBaseAttackDamageMod;
+            double damage = 0;
+
+            switch (slot)
+            {
+                case SpellSlot.Q:
+                    //Damage caused each upgrade                             AD/AP Scaling
+                    damage += new[] { 10, 20, 30, 40, 50 }[slotLevel] + (0.10 * ad);
+                    break;
+                case SpellSlot.W:
+                    //Damage caused each upgrade                             AD/AP Scaling
+                    damage += new[] { 10, 20, 30, 40, 50 }[slotLevel] + (0.10 * ap);
+                    break;
+                case SpellSlot.E:
+                    //Damage caused each upgrade                             AD/AP Scaling
+                    damage += new[] { 10, 20, 30, 40, 50 }[slotLevel] + (0.10 * ad);
+                    break;
+                case SpellSlot.R:
+                    //Damage caused each upgrade                   AD/AP Scaling
+                    damage += new[] { 10, 20, 30 }[slotLevel] + (0.10 * ap);
+                    break;
+            } //                                                                      -10 Cause we need to add also the health regen. So we give an approximate of 10
+            return ObjectManager.Player.CalcDamage(target, Damage.DamageType.Magical, damage - 10);
+        }
+
+        public static double GetTotalDamage(this Obj_AI_Base target)
+        {
+            var ap = ObjectManager.Player.FlatMagicDamageMod;
+            var ad = ObjectManager.Player.FlatBaseAttackDamageMod;
+            double damage = 0;
+            
+            //Damage caused each upgrade                        AD/AP Scaling
+            damage += new[] { 10, 20, 30, 40, 50 }[Q.Level - 1] + (0.10 * ad);
+            //Damage caused each upgrade                        AD/AP Scaling
+            damage += new[] { 10, 20, 30, 40, 50 }[W.Level - 1] + (0.10 * ap);
+            //Damage caused each upgrade                        AD/AP Scaling
+            damage += new[] { 10, 20, 30, 40, 50 }[E.Level - 1] + (0.10 * ad);
+            //Damage caused each upgrade                AD/AP Scaling
+            damage += new[] { 10, 20, 30 }[R.Level - 1] + (0.10 * ap);
+            //                                                                      -10 Cause we need to add also the health regen. So we give an approximate of 10
+            return ObjectManager.Player.CalcDamage(target, Damage.DamageType.Magical, damage);
         }
     }
 }
